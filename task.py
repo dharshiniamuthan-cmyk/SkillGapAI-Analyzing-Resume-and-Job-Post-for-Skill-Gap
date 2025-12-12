9e4f126852a5c7518cdbb880d3eb7d456aa1754c
dharshiniamuthan-cmyk <dharshiniamuthan@gmail.com>
2025-12-12 20:42:27 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,127 @@
+
+1)print("Hello SkillGapAI")
+
+
+2)name = "Your Name"
+print(name)
+
+3)age = 25
+city = "Chennai"
+skill = "Python"
+print(age, city, skill)
+
+
+4)value = str(100)
+print(type(value))
+
+
+5)text = "python basics"
+print(text.upper())
+
+
+
+6)print(len("SkillGapAI"))
+
+
+
+7)colors = ['red', 'blue', 'green']
+print(colors[1])
+
+8)fruits = ['apple', 'banana']
+fruits.append('orange')
+print(fruits)
+
+
+
+9)items = ['apple', 'banana', 'orange']
+items.remove('banana')
+print(items)
+
+
+
+10)nums = [1, 2, 3]
+t = tuple(nums)
+print(t)
+
+
+
+11)resume = {'name': 'John', 'age': 25, 'skills': ['Python', 'SQL']}
+print(resume)
+
+
+
+12)print(resume['skills'])
+
+
+13)marks = 50
+print('Pass' if marks > 40 else 'Fail')
+
+
+14)num = 7
+print('Even' if num % 2 == 0 else 'Odd')
+
+
+15)for i in range(1, 11):
+    print(i)
+
+
+
+16)i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+17)def say_hello():
+    print("Hello")
+   say_hello()
+
+
+
+18)def add(a, b):
+    return a + b
+print(add(5, 3))
+
+
+
+19)import math
+print(math.pi)
+
+
+OUTPUTS
+
+
+Hello SkillGapAI
+Your Name
+25 Chennai Python
+<class 'str'>
+PYTHON BASICS
+10
+blue
+['apple', 'banana', 'orange']
+['apple', 'orange']
+(1, 2, 3)
+{'name': 'John', 'age': 25, 'skills': ['Python', 'SQL']}
+['Python', 'SQL']
+Pass
+Odd
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+5
+4
+3
+2
+1
+Hello
+8
+3.141592653589793
+
+
